1a2c28d8926bf8e9c8ba17c96ab5a3f01265ad6d
jasonleyser <jason.leyser@protocol.ai>
2021-10-16 13:33:11 -0600

    Update [...play].js

--- pages/api/radio/[...play].js
+++ pages/api/radio/[...play].js
@@ -1,12 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from '../../../api'
 
 async function submitData(props) {	
-	const supabase = createClient(
-		'https://pnkzceroksigqqmtsaja.supabase.co',
-		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNjc0OTM5NywiZXhwIjoxOTQyMzI1Mzk3fQ.X_Ga_qrcpq67GDaZt4th9QmVJreARB2jS1mypy7nJ4A'
-	)
-
 	//let main = decodeURIComponent(props.url)
 	//console.log('main', main)
 
